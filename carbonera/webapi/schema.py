485cb6ede768b0fd725fffe352836d138e7032e8
Yacine Alami <yacine.alami@gmail.com>
2021-12-13 10:31:34 +0100

    Added more resolvers

--- carbonera/webapi/schema.py
+++ carbonera/webapi/schema.py
@@ -1,6 +1,14 @@
 from graphene_django import DjangoObjectType
 import graphene
-from webapi.models import Unit 
+from webapi.models import Unit, Item, Category 
+
+class ItemsType(DjangoObjectType):
+    class Meta:
+        model = Item
+
+class CategoriesType(DjangoObjectType):
+    class Meta:
+        model = Category
 
 class UnitsType(DjangoObjectType):
     class Meta:
@@ -8,8 +16,16 @@ class UnitsType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_units = graphene.List(UnitsType)
+    all_items = graphene.List(ItemsType)
+    all_categories = graphene.List(CategoriesType)
 
     def resolve_all_units(self, info):
         return Unit.objects.all()
+
+    def resolve_all_items(self, info):
+        return Item.objects.all()
+
+    def resolve_all_categories(self, info):
+        return Category.objects.all()
 
 schema = graphene.Schema(query=Query)
